29e9c12c0c6e3abded5cacb1b7c11835975d3dbb
gwise <gwise@GWISE-PC>
2020-10-08 08:18:50 +0900

    Add project files.

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            SecondPage secondPage = new SecondPage();
+            secondPage.ReturnInfoEvent += SecondPage_ReturnInfoEvent;
+
+            await Navigation.PushAsync(secondPage);
+        }
+
+        private void SecondPage_ReturnInfoEvent(string returnInfo)
+        {
+            this.SaveButton.Text = returnInfo;
+        }
+    }
+}
---
+++ App2/App2/SecondPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    public delegate void ReturnInfoEventHandler(string returnInfo);
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SecondPage : ContentPage
+    {
+
+        public event ReturnInfoEventHandler ReturnInfoEvent;
+
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            //if(ReturnInfoEvent != null)
+            //{
+            //    ReturnInfoEvent(this.UserID.Text);
+            //}
+
+            ReturnInfoEvent?.Invoke(this.UserID.Text);
+
+
+            //SecondPage 화면 종료.
+            await Navigation.PopAsync();
+        }
+    }
+}
